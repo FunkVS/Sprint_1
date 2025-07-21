d1393b858ac84ad11c8ae337c59afdf967bfa58e
FunkVS <24061999@mail.ru>
2025-07-21 17:32:21 +0700

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -2,19 +2,15 @@ time_values = '1h 45m,360s,25m,30m 120s,2h 60s'
 total_minutes = 0
 
 for time_part in time_values.split(','):
-    # Разбиваем каждую часть на отдельные компоненты времени
-    for component in time_part.split():
-        if 'h' in component:
-            # Преобразуем часы в минуты
-            hours = int(component.replace('h', ''))
+    for item in time_part.split():
+        if 'h' in item:
+            hours = int(item.replace('h', ''))
             total_minutes += hours * 60
-        elif 'm' in component:
-            # Добавляем минуты
-            minutes = int(component.replace('m', ''))
+        elif 'm' in item:
+            minutes = int(item.replace('m', ''))
             total_minutes += minutes
-        elif 's' in component:
-            # Преобразуем секунды в минуты
-            seconds = int(component.replace('s', ''))
+        elif 's' in item:
+            seconds = int(item.replace('s', ''))
             total_minutes += seconds // 60
 
 print(f"Общее количество минут: {total_minutes}")
